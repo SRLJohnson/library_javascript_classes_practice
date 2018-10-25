6a761da7d6b953507e6cfbfb5a9c6abb9296c1d0
SRLJohnson <43064514+SRLJohnson@users.noreply.github.com>
2018-10-24 20:22:44 -0400

    added books, movies, CD classes

--- library.js
+++ library.js
@@ -24,4 +24,56 @@ class Media {
   toggleCheckedOutStatus() {
     this._isCheckedOut = !this._isCheckedOut;
   }
+
+  addRating(newRating) {
+    this._ratings.push(newRating);
+  }
+}
+
+class Books extends Media {
+  constructor(title, author, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+}
+
+class Movie extends Media {
+  constructor(title, director, runtimeInMinutes) {
+    super(title);
+    this._director = director;
+    this._runtimeInMinutes = runtimeInMinutes;
+  }
+
+  get director() {
+    return this._director;
+  }
+
+  get runtimeInMinutes() {
+    return this._runtimeInMinutes;
+  }
+}
+
+class CD extends Media {
+  constructor(title, artist, songs) {
+    super(title);
+    this._artist = artist;
+    this._songs = songs;
+  }
+
+  get artist() {
+    return this._artist;
+  }
+
+  get songs() {
+    return this._songs;
+  }
 }
